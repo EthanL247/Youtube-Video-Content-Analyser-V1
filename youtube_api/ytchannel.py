852c507fc77f2e465a74215fcedad8f9b026a9ab
EthanL247 <Ethanliu@Live.co.uk>
2023-09-27 20:32:04 +0100

    Update ytchannel.py

--- youtube_api/ytchannel.py
+++ youtube_api/ytchannel.py
@@ -8,7 +8,8 @@ class Ytchannel:
     def __init__(self):
         self.name = 'youtube'
         self.service = 'v3'
-        self.api_key = 'AIzaSyDMg2PyJVlG9sj79VXnlffmlD86wEHzXxI'
+        #Enter your own youtube API Key
+        self.api_key = 'ENTER YOUR YOUTUBE API KEY'
     
     def _start(self) -> object:
         """ Returns youtube service object """ 
